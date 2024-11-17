9062b185c9dec45012443dfdb108204fe8b293a7
sbendavid <samuelbendavid01@gmail.com>
2024-11-17 22:20:05 +0100

    update: built a stopwatch

---
+++ stop watch/main.py
@@ -0,0 +1,15 @@
+import time
+print("Press enter to start...")
+print("Press ctrl+c to cancel...")
+
+while True:
+    try:
+        input()
+        start_time = time.time()
+        print("Stopwatch started ...!")
+    except:
+        print("Stopwatch stopped ...!")
+        end_time = time.time()
+        print("Total time: ", round(end_time - start_time, 2), " seconds")
+        break
+    
